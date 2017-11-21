c72f6b79b1eadcc16f731d020a0f93e1f62885f4
Brian Stamper <brian.stamper@chrr.osu.edu>
2017-11-21 16:31:31 -0500

    Some rearranging, identifying which data can be used overall

--- modeling.R
+++ modeling.R
@@ -1,20 +1,7 @@
-
 library(data.table)
-
 
 load('data/processed/ohiolmi_occ.rds')
-load('data/processed/onet_knowledge.rds')
-
-ohiolmi_occ[, high_growth := occ_empl_change_pct_lmi > .1]
-
-# Known issue of not all SOCs match up, ignoring -- can't do much about it
-knowledge_by_soc <- merge(onet_knowledge, 
-                          subset(ohiolmi_occ, select = c('soc_code_lmi', 'high_growth')), 
-                          by.x = 'soc_code', 
-                          by.y = 'soc_code_lmi')
-
-
-library(data.table)
+load('data/processed/onet_ksaw.rds')
 
 # Entropy function for a single variable
 entropy <- function(x) {
@@ -30,13 +17,145 @@ info_gain <- function(x, y) {
   entropy(x) - p1y * entropy(x[y]) - p2y * entropy(x[!y])
 }
 
-# Values based on the slide
-k1 <- data.table(row = 1:59, murder = TRUE, gun = TRUE)
-k2 <- data.table(row = 1:57, murder = TRUE, gun = FALSE)
-k3 <- data.table(row = 1:59, murder = FALSE, gun = TRUE)
-k4 <- data.table(row = 1:75, murder = FALSE, gun = FALSE)
+# Define high growth as > 10%
+ohiolmi_occ[, high_growth := occ_empl_change_pct_lmi > .1]
 
-mg <- rbindlist(list(k1, k2, k3, k4))
 
-entropy(mg$murder)
-info_gain(mg$murder, mg$gun)
+for(im_break in 4:8/10) {
+  for(lv_break in 4:8/10) {
+    
+    
+    onet_ksaw <- rbindlist(list(onet_knowledge, onet_skills, onet_abilities, onet_work_activities))
+    
+    
+    # Separate the six-digit SOC and filter for cases where the suffix == 00 (the aggregate)
+    onet_ksaw[, soc_code := substr(`O*NET-SOC Code`, 1, 7)]
+    onet_ksaw[, soc_suff := substr(`O*NET-SOC Code`, 9, 10)]
+    onet_ksaw <- onet_ksaw[soc_suff == '00']
+    
+    # rename and select relevant variables
+    setnames(onet_ksaw,
+             old = c('Element ID',
+                     'Element Name',
+                     'Scale ID',
+                     'Data Value'),
+             new = c('elem_id',
+                     'elem_name',
+                     'scale_id',
+                     'value'))
+    
+    onet_ksaw <- subset(onet_ksaw,
+                        select = c('soc_code',
+                                   'elem_id',
+                                   'elem_name',
+                                   'scale_id',
+                                   'value'))
+    
+    # Cast the 'importance' next to the 'level' for each soc/knowledge
+    onet_ksaw <- dcast(onet_ksaw, soc_code + elem_id + elem_name ~ scale_id, value.var = 'value')
+    
+    # For each occupation, we want to know if a given knowledge area is
+    # in the upper quantile for importance (Scale ID = 'IM') *AND* 
+    # competency required (Scale ID = 'LV')
+    # Compute with the upper quantile break point:
+    # (using quantile because experimenting with different break points)
+    onet_ksaw[, high_IM := quantile(IM, im_break), by = 'elem_id']
+    onet_ksaw[, high_LV := quantile(LV, lv_break), by = 'elem_id']
+    
+    # Determine if both importance and level are above both for the occupation:
+    onet_ksaw[, high_IMLV := IM > high_IM & LV > high_LV]
+    
+    # Cast the score wide over the knowledge areas:
+    onet_ksaw <- dcast(onet_ksaw, soc_code ~ elem_id, value.var = 'high_IMLV')
+    
+    
+    
+    # Known issue of not all SOCs match up, ignoring -- can't do much about it
+    ksaw_by_soc <- merge(onet_ksaw, 
+                         subset(ohiolmi_occ, select = c('soc_code_lmi', 'high_growth')), 
+                         by.x = 'soc_code', 
+                         by.y = 'soc_code_lmi')
+    
+    # soc_code is the first variable now, high_growth the last
+    ksaw_values <- ksaw_by_soc[, -c('soc_code', 'high_growth')]
+    
+    information_gain <- sapply(ksaw_values, function(this_value) {
+      info_gain(ksaw_by_soc$high_growth, this_value)
+    })
+    
+    information_gain <- data.table(elem_id = names(information_gain),
+                                   elem_ig = information_gain)
+    
+    ig_explorer <- merge(information_gain, onet_content_model)
+    top_ig <- ig_explorer[order(-frank(elem_ig))]
+    message('im_break: ', im_break)
+    message('lv_break: ', lv_break)
+    message(round(top_ig$elem_ig[3], 2)) 
+    
+  }
+}
+
+#
+#onet_ksaw <- rbindlist(list(onet_knowledge, onet_skills, onet_abilities, onet_work_activities))
+#
+#
+## Separate the six-digit SOC and filter for cases where the suffix == 00 (the aggregate)
+#onet_ksaw[, soc_code := substr(`O*NET-SOC Code`, 1, 7)]
+#onet_ksaw[, soc_suff := substr(`O*NET-SOC Code`, 9, 10)]
+#onet_ksaw <- onet_ksaw[soc_suff == '00']
+#
+## rename and select relevant variables
+#setnames(onet_ksaw,
+#         old = c('Element ID',
+#                 'Element Name',
+#                 'Scale ID',
+#                 'Data Value'),
+#         new = c('elem_id',
+#                 'elem_name',
+#                 'scale_id',
+#                 'value'))
+#
+#onet_ksaw <- subset(onet_ksaw,
+#                    select = c('soc_code',
+#                               'elem_id',
+#                               'elem_name',
+#                               'scale_id',
+#                               'value'))
+#
+## Cast the 'importance' next to the 'level' for each soc/knowledge
+#onet_ksaw <- dcast(onet_ksaw, soc_code + elem_id + elem_name ~ scale_id, value.var = 'value')
+#
+## For each occupation, we want to know if a given knowledge area is
+## in the upper quantile for importance (Scale ID = 'IM') *AND* 
+## competency required (Scale ID = 'LV')
+## Compute with the upper quantile break point:
+## (using quantile because experimenting with different break points)
+#onet_ksaw[, high_IM := quantile(IM, im_break), by = 'elem_id']
+#onet_ksaw[, high_LV := quantile(LV, lv_break), by = 'elem_id']
+#
+## Determine if both importance and level are above both for the occupation:
+#onet_ksaw[, high_IMLV := IM > high_IM & LV > high_LV]
+#
+## Cast the score wide over the knowledge areas:
+#onet_ksaw <- dcast(onet_ksaw, soc_code ~ elem_id, value.var = 'high_IMLV')
+#
+#
+#
+## Known issue of not all SOCs match up, ignoring -- can't do much about it
+#ksaw_by_soc <- merge(onet_ksaw, 
+#                     subset(ohiolmi_occ, select = c('soc_code_lmi', 'high_growth')), 
+#                     by.x = 'soc_code', 
+#                     by.y = 'soc_code_lmi')
+#
+## soc_code is the first variable now, high_growth the last
+#ksaw_values <- ksaw_by_soc[, -c('soc_code', 'high_growth')]
+#
+#information_gain <- sapply(ksaw_values, function(this_value) {
+#  info_gain(ksaw_by_soc$high_growth, this_value)
+#})
+#
+#information_gain <- data.table(elem_id = names(information_gain),
+#                               elem_ig = information_gain)
+#
+#ig_explorer <- merge(information_gain, onet_content_model)
+#
--- onet-read.R
+++ onet-read.R
@@ -1,21 +1,77 @@
+## O*Net data preparation
+library(readxl)
+library(data.table)
+
+if(!dir.exists('data/onet')) dir.create('data/onet')
+if(!dir.exists('data/processed')) dir.create('data/processed')
+
+# Downloader function to save repetitive code and file transfers
+onet_loader <- function(fn, ...) {
+  if(!file.exists(paste0('data/onet/', fn))) {
+    download.file(url = paste0('https://www.onetcenter.org/dl_files/database/db_22_1_text/', fn),
+                  destfile = paste0('data/onet/', fn),
+                  mode = 'wb')
+  }
+  fread(paste0('data/onet/', fn), na.strings = 'n/a', ...)
+}
+
+onet_content_model <- onet_loader('Content%20Model%20Reference.txt')
+names(onet_content_model) <- c('elem_id', 'elem_name', 'elem_desc')
+save(onet_content_model, file = 'data/processed/onet_content_model.rds')
 
 
-#Knowledge, Skills, Abilities
-onet_knowledge <- fread('https://www.onetcenter.org/dl_files/database/db_22_1_text/Knowledge.txt', na.strings = 'n/a')
-onet_skills <- fread('https://www.onetcenter.org/dl_files/database/db_22_1_text/Skills.txt', na.strings = 'n/a')
-onet_abilities <- fread('https://www.onetcenter.org/dl_files/database/db_22_1_text/Abilities.txt', na.strings = 'n/a')
 
-#Education, Experience, Training
-onet_educ <- fread('https://www.onetcenter.org/dl_files/database/db_22_1_text/Education%2C%20Training%2C%20and%20Experience.txt', na.strings = 'n/a')
-onet_educ_cats <- fread('https://www.onetcenter.org/dl_files/database/db_22_1_text/Education%2C%20Training%2C%20and%20Experience%20Categories.txt', na.strings = 'n/a')
-onet_job_zones <- fread('https://www.onetcenter.org/dl_files/database/db_22_1_text/Job%20Zones.txt', na.strings = 'n/a')
-onet_job_zone_ref <- fread('https://www.onetcenter.org/dl_files/database/db_22_1_text/Job%20Zone%20Reference.txt', na.strings = 'n/a')
+# data with IM/LV
+onet_knowledge <- onet_loader('Knowledge.txt')
+onet_skills <- onet_loader('Skills.txt')
+onet_abilities <- onet_loader('Abilities.txt')
+onet_work_activities <- onet_loader('Work%20Activities.txt')
 
-#Interests, Work Values, Work Styles
-onet_interests <- fread('https://www.onetcenter.org/dl_files/database/db_22_1_text/Interests.txt', na.strings = 'n/a')
-onet_work_values <- fread('https://www.onetcenter.org/dl_files/database/db_22_1_text/Work%20Values.txt', na.strings = 'n/a')
-onet_work_styles <- fread('https://www.onetcenter.org/dl_files/database/db_22_1_text/Work%20Styles.txt', na.strings = 'n/a')
+onet_ksaw <- rbindlist(list(onet_knowledge, onet_skills, onet_abilities, onet_work_activities))
 
-#Work Context
-onet_work_context <- fread('https://www.onetcenter.org/dl_files/database/db_22_1_text/Work%20Context.txt', na.strings = 'n/a')
-onet_work_context_cats <- fread('https://www.onetcenter.org/dl_files/database/db_22_1_text/Work%20Context%20Categories.txt', na.strings = 'n/a')
+
+# Separate the six-digit SOC and filter for cases where the suffix == 00 (the aggregate)
+onet_ksaw[, soc_code := substr(`O*NET-SOC Code`, 1, 7)]
+onet_ksaw[, soc_suff := substr(`O*NET-SOC Code`, 9, 10)]
+onet_ksaw <- onet_ksaw[soc_suff == '00']
+
+# rename and select relevant variables
+setnames(onet_ksaw,
+         old = c('Element ID',
+                 'Element Name',
+                 'Scale ID',
+                 'Data Value'),
+         new = c('elem_id',
+                 'elem_name',
+                 'scale_id',
+                 'value'))
+
+onet_ksaw <- subset(onet_ksaw,
+                         select = c('soc_code',
+                                    'elem_id',
+                                    'elem_name',
+                                    'scale_id',
+                                    'value'))
+
+# Cast the 'importance' next to the 'level' for each soc/knowledge
+onet_ksaw <- dcast(onet_ksaw, soc_code + elem_id + elem_name ~ scale_id, value.var = 'value')
+
+# For each occupation, we want to know if a given knowledge area is
+# in the upper quantile for importance (Scale ID = 'IM') *AND* 
+# competency required (Scale ID = 'LV')
+# Compute with the upper quantile break point:
+# (using quantile because experimenting with different break points)
+onet_ksaw[, high_IM := quantile(IM, im_break), by = 'elem_id']
+onet_ksaw[, high_LV := quantile(LV, lv_break), by = 'elem_id']
+
+# Determine if both importance and level are above both for the occupation:
+onet_ksaw[, high_IMLV := IM > high_IM & LV > high_LV]
+
+# Cast the score wide over the knowledge areas:
+onet_ksaw <- dcast(onet_ksaw, soc_code ~ elem_id, value.var = 'high_IMLV')
+
+#save(onet_ksaw, file = 'data/processed/onet_ksaw.rds')
+
+
+
+
